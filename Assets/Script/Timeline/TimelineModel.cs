82f0f1cf4cb911672730b776f9e9ca4228c36aa0
SBKing2 <1938957372@qq.com>
2025-07-16 19:27:45 +0800

    <feat>(Data) : Timeline System

---
+++ Assets/Script/Timeline/TimelineModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+public delegate void onTimelineNode(TimelineObj obj, params object[] args);     //TimelineNode事件的委托
+
+public struct TimelineNode
+{
+    public TimelineNode(float timeElapsed, string funcName, params object[] args)
+    {
+        this.timeElapsed = timeElapsed;
+        this.event_args = args;
+        this.onEventOccur = null;    //TODO:从脚本字典里面找到函数并赋值
+    }
+    public float timeElapsed;
+    public object[] event_args;
+    public onTimelineNode onEventOccur;
+}
+
+public struct TimelineModel
+{
+    public TimelineModel(string id, float duration, List<TimelineNode> nodes)
+    {
+        this.id = id;
+        this.duration = duration;
+        this.timeline_nodes = nodes;
+    }
+
+    public string id;
+    public float duration;      //持续时间
+    public List<TimelineNode> timeline_nodes;
+}
---
+++ Assets/Script/Timeline/TimelineObj.cs
@@ -0,0 +1,9 @@
+﻿
+using UnityEngine;
+
+public struct TimelineObj
+{
+    public TimelineModel model;
+    public GameObject caster;   //施法者
+    public float timeElapsed;   //已经进行的时间
+}
